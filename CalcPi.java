8c561bc18177ce21d4f4bfaca982f9dad957bc34
ReutHaimi <reut.haimi@post.runi.ac.il>
2025-11-06 21:56:36 +0200

    creating a calcultor fot pi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String args[]) {
+	int index = Integer.parseInt(args[0]);
+	double nextNumber = 3;
+	double sum = 1;
+	boolean isPositive = false;
+	while (index > 0){
+		if (isPositive == false){
+			sum = sum - (1 / nextNumber);
+		} else { 
+			sum = sum + (1 / nextNumber);
+		} 
+		System.out.println(sum);
+		isPositive = !isPositive;
+		nextNumber += 2;
+		index --;
+	}
+	System.out.println("pi, according to Java: " + Math.PI);
+	System.out.println("pi, approximated: " + sum * 4);
 	}
 }
